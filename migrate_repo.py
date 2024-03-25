de3da5a563ad0b14b06d6913d6241762256daa5e
Chris Taylor <chris11.taylor@nttdata.com>
2024-03-25 15:09:40 -0500

    Allow bypass on "go mod tidy" step

--- migrate_repo.py
+++ migrate_repo.py
@@ -339,7 +339,7 @@ def dynamic_replacements(source_repo: Repo):
 
     if updated_go_module or updated_go_version or should_tidy:
         delete_if_present(file_path=source_repo_path.joinpath("go.sum"))
-        _ = block_for_user_input(
+        _ = block_for_user_input_with_bypass(
             message="Executing `go mod tidy`",
             retry_function=shell_command,
             command=["go", "mod", "tidy"],
